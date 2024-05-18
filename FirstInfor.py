96aedd8c46a10c82bafc38be2b7150845bbaef07
Jitterkkk <jitterzibetti@gmail.com>
2024-05-18 12:34:48 -0300

    Primeiros passos

---
+++ FirstInfor.py
@@ -0,0 +1,31 @@
+#importar as bibliotecas
+from tkinter import *
+from tkinter import messagebox
+
+#Criar nossa janela
+
+Janela = Tk()
+Janela.title("Castel's Barbearia - Painel de acesso")
+Janela.geometry("600x300")
+Janela.configure(background="white")
+#Faz com que a janela não possa mudar sua propriedade
+#Não tem necessidade de ser responsivo
+Janela.resizable(width=False, height=False)
+
+#============== Carregando Imagens =============
+logo = PhotoImage()
+
+
+#============== Widgests ===============
+LeftFrame = Frame(Janela, width=200, height=300, bg="MIDNIGHTBLUE", relief="raise")
+LeftFrame.pack(side=LEFT)
+
+RightFrame = Frame(Janela, width=395, height=300, bg="MIDNIGHTBLUE", relief="raise")
+RightFrame.pack(side=RIGHT)
+
+
+
+
+Janela.mainloop()
+
+
